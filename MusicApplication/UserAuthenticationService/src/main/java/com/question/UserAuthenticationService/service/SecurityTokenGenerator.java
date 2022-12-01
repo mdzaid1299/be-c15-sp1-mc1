fde1bdfcfd6f65d62beaaf2d41bafa9acca91797
HP <mdzaid1299@gmail.com>
2022-12-01 11:25:24 +0530

    ini

---
+++ MusicApplication/UserAuthenticationService/src/main/java/com/question/UserAuthenticationService/service/SecurityTokenGenerator.java
@@ -0,0 +1,10 @@
+package com.question.UserAuthenticationService.service;
+
+import com.question.UserAuthenticationService.domain.User;
+
+import java.util.Map;
+
+public interface SecurityTokenGenerator {
+    Map<String,String> generateToken(User user);
+
+}
